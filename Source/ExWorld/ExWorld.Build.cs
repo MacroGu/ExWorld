700c3c9f4abb1cb0f465d7d65323638d5dcccefc
Hong Gu <macrogu@qq.com>
2022-04-04 11:48:48 +0800

    fixed: 1: add public/private folders under source

--- Source/ExWorld/ExWorld.Build.cs
+++ Source/ExWorld/ExWorld.Build.cs
@@ -7,7 +7,14 @@ public class ExWorld : ModuleRules
 	public ExWorld(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
+		PublicIncludePaths.AddRange(
+			new string[] {
+						// ... add public include paths required here ...
+						ModuleDirectory,
+						"ExWorld/Public",
+						"ExWorld/Public/UI"
+			}
+		);
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
 	}
 }
